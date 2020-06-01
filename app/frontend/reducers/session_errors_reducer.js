06521f076f1bd49cc03966e12153e00335f97441
ericsmith9161 <ericsmith9161@gmail.com>
2020-06-01 13:54:30 -0400

    Create session_errors_reducer

---
+++ app/frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,17 @@
+import {
+  RECEIVE_SESSION_ERRORS,
+  RECEIVE_CURRENT_USER
+} from '../actions/session_actions';
+
+export default (errors = [], action) => {
+
+  switch (action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      console.log(action.errors);
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return [];
+    default:
+      return errors;
+  }
+}
