0cb7d18b5dc7d0cc2ccecbe5b497b546a80f49c2
ericsmith9161 <ericsmith9161@gmail.com>
2020-06-01 14:00:55 -0400

    Add redux logger to packages

--- app/frontend/szyslak.jsx
+++ app/frontend/szyslak.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {configureStore} from '../frontend/store/store'
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+  window.store = store;
   ReactDOM.render(<h1>Szyslak</h1>, root);
 });
