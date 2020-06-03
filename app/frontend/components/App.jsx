a6e9c4b1c59977d5a8077a40fd1f0e8887c6d55e
ericsmith9161 <ericsmith9161@gmail.com>
2020-06-02 21:09:39 -0400

    Add icons to Splash page, make display dynamic

--- app/frontend/components/App.jsx
+++ app/frontend/components/App.jsx
@@ -11,9 +11,6 @@ import SidebarContainer from "./sidebar/SidebarContainer";
 
 const App = () => (
   <div>
-    <header>
-        <h1>Szyslak</h1>
-    </header>
       <AuthRoute exact path="/greeting" component={GreetingContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
