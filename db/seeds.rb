f1abb5251694ab99514d7b3ebe75f7f23467a014
ericsmith9161 <ericsmith9161@gmail.com>
2020-06-05 01:07:14 -0400

    changed seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+demo_user = User.Create!(email: 'demoaccount@demo.demo', username: 'Demo User', password: 'password')
 
 admin = User.create!(email: 'admin@admin.admin', username: 'admin', password: 'password')
 
 index_channel = Channel.create!(name: 'Main', details: 'Hang out here or check out some other channels', creator_id: admin.id)
-
 Subscription.create!({user_id: demo_user.id, subscribeable: index_channel})
