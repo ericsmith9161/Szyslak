f4f447a8fe89783fb65daff07f8ab63d47eef278
ericsmith9161 <ericsmith9161@gmail.com>
2020-06-06 13:55:05 -0400

    Create thunk action creators for fetching message, fetching all messages

---
+++ app/frontend/actions/message_actions.js
@@ -0,0 +1,26 @@
+import * as APIUtil from '../util/message_api_util'
+
+export const RECEIVE_MESSAGES = 'RECEIVE_MESSAGES';
+export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
+
+const receiveMessages = (messages) => {
+  return {
+    type: RECEIVE_MESSAGES,
+    messages
+  }
+}
+
+export const fetchMessages = () => (dispatch) => {
+  return APIUtil.fetchMessages().then(messages => dispatch(receiveMessages(messages)));
+}
+
+const receiveMessage = (message) => {
+  return {
+    type: RECEIVE_CHANNEL,
+    message
+  }
+}
+
+export const fetchMessage = (messageId) => (dispatch) => {
+  return APIUtil.fetchMessage(messageId).then(message => dispatch(receiveMessage(message)));
+}
